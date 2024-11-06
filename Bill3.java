29700ac9866caa404499a7dc5e227afb59734981
Daniel Malede <danielmalede@gmail.com>
2024-11-06 21:58:03 +0200

    Bill

--- Bill3.java
+++ Bill3.java
@@ -3,7 +3,7 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 		// Replace this comment with the rest of your code
-		
+
 		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
@@ -11,7 +11,6 @@ public class Bill3 {
 
 		Double billingSplit = bill / 3;
 		billingSplit = Math.ceil(billingSplit);
-
 		System.out.println("Dear " + name3 + ", " + name2 + ", " + name1 + ": pay " + billingSplit + " Shekels each.");
 	}
 }
