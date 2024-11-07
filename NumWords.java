8ca937263df614737fb28c8c6298d62781985ea4
Daniel Malede <danielmalede@gmail.com>
2024-11-07 09:24:07 +0200

    NumWords

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,30 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		// Replace this comment with your code
+		String number = args[0];
+		if (number.length() == 3) {
+			{
+				System.out.println(number.charAt(0) + " hundreds, " + number.charAt(1) + " tens, " + "and "
+						+ number.charAt(2) + " ones.");
+				return;
+			}
+		}
+		if (number.length() == 2) {
+			{
+				System.out
+						.println("0 hundreds, " + number.charAt(0) + " tens, " + "and " + number.charAt(1) + " ones.");
+
+				return;
+			}
+		}
+		if (number.length() == 1) {
+			{
+				System.out
+						.println("0 hundreds, " + "0 tens, " + number.charAt(0) + " and " + "ones.");
+				return;
+			}
+		}
+		System.out.println("Please enter a valid number between 1 and 999.");
 	}
 }
